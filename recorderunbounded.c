ae8890e73d4d29d1146068a8daf2a6084f450e42
Kyle Peter Fritz <kylefrit@amazon.com>
2016-08-03 17:15:20 +0000

    Add wave file write.

--- recorderunbounded.c
+++ recorderunbounded.c
@@ -5,13 +5,88 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <wiringPi.h>
 #include <mcp3004.h>
+#include <string.h>
 
 #define BASE 100
 #define SPI_CHANNEL 0
 #define ADC_CHANNEL 7
+
+// wav file builder - begin
+#define WAVFILE_SAMPLES_PER_SECOND 23415
+
+struct wavfile_header {
+	char	riff_tag[4];
+	int	    riff_length;
+	char	wave_tag[4];
+	char	fmt_tag[4];
+	int	    fmt_length;
+	short	audio_format;
+	short	num_channels;
+	int	    sample_rate;
+	int	    byte_rate;
+	short	block_align;
+	short	bits_per_sample;
+	char	data_tag[4];
+	int	    data_length;
+};
+
+FILE * wavfile_open( const char *filename, int samplerate)
+{
+	struct wavfile_header header;
+
+	int samples_per_second = samplerate;
+	int bits_per_sample = 16;
+
+	strncpy(header.riff_tag,"RIFF",4);
+	strncpy(header.wave_tag,"WAVE",4);
+	strncpy(header.fmt_tag,"fmt ",4);
+	strncpy(header.data_tag,"data",4);
+
+	header.riff_length = 0;
+	header.fmt_length = 16;
+	header.audio_format = 1;
+	header.num_channels = 1;
+	header.sample_rate = samples_per_second;
+	header.byte_rate = samples_per_second*(bits_per_sample/8);
+	header.block_align = bits_per_sample/8;
+	header.bits_per_sample = bits_per_sample;
+	header.data_length = 0;
+
+	FILE * file = fopen(filename,"w+");
+	if(!file) return 0;
+
+	fwrite(&header,sizeof(header),1,file);
+
+	fflush(file);
+
+	return file;
+
+}
+
+void wavfile_write( FILE *file, short data[], int length )
+{
+	fwrite(data,sizeof(short),length,file);
+}
+
+void wavfile_close( FILE *file )
+{
+	int file_length = ftell(file);
+
+	int data_length = file_length - sizeof(struct wavfile_header);
+	fseek(file,sizeof(struct wavfile_header) - sizeof(int),SEEK_SET);
+	fwrite(&data_length,sizeof(data_length),1,file);
+
+	int riff_length = file_length - 8;
+	fseek(file,4,SEEK_SET);
+	fwrite(&riff_length,sizeof(riff_length),1,file);
+
+	fclose(file);
+}
+// wav file builder -end
 
 void timespec_diff(struct timespec *start, struct timespec *stop,
                    struct timespec *result)
@@ -29,6 +104,19 @@ void timespec_diff(struct timespec *start, struct timespec *stop,
 	return;
 }
 
+void input2wavbytes(int* input, short* wav, int sz)
+{
+	if(!wav)
+	{
+		wav = malloc(sz*sizeof(short));
+	}
+	// need to add the logic to convert 
+	// temporarily copying input to wav
+	
+	memcpy(wav, input, sz*sizeof(int));
+	
+}
+
 int main(int argc, char **argv)
 {
 	wiringPiSetup();
@@ -37,7 +125,7 @@ int main(int argc, char **argv)
 	int sampleCount = 22 * 1000 * 10;
 	
 	int index;
-	int values[sampleCount];
+	short  values[sampleCount];
 	struct timespec start, end, duration;
 	clock_gettime(CLOCK_REALTIME, &start);
 	for (index = 0; index < sampleCount; index++)
@@ -50,10 +138,22 @@ int main(int argc, char **argv)
 	printf("duration: %lld.%.9ld\n", (long long)duration.tv_sec, duration.tv_nsec);
 	printf("sample rate: %f\n", sampleRateHz);
 	
+	FILE* file = wavfile_open("sound.wav",(int)sampleRateHz);//)WAVFILE_SAMPLES_PER_SECOND);
+	if(!file)
+	{
+		printf("Could not open file sound.wav");
+		return 0;
+	}
+
+	//printf("\nsize of short %d, Int %d", sizeof(short), sizeof(int));
+	//wav create end 
 	for (index = 0; index < sampleCount; index++)
 	{
-		printf("%i\n", values[index]);
+		//printf("%i\n", values[index]);
+		values[index] += 500;
 	}
+	wavfile_write(file, values, sampleCount);
+	wavfile_close(file);
 	
 	return 0;
 }
